a831937444282becec48f7d3a2dbe24dec962962
Pandomimcafe <obariscem@gmail.com>
2025-06-03 18:24:33 +0300

    Add files via upload

---
+++ compliments_inline_script.js
@@ -0,0 +1,13 @@
+<script>
+  window.storyCompliments = [
+"🌟 Senin enerjin etrafına ışık saçıyor!",
+"🌈 Gülüşün, karanlık bir günü aydınlatabilir.",
+"✨ Bugün harika şeyler seninle olacak!"
+  ];
+
+  window.makeupCompliments = [
+"💄 Gözlerin, yıldızlarla yarışıyor.",
+"🪞 Bugün güzelliğin sınır tanımıyor.",
+"🌹 Zarifliğinle çevreni büyülüyorsun."
+  ];
+</script>
